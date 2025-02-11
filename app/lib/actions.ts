2211be701d59923f9e07ece5bc9c3d97c4753ac7
adelagranados <55199072+adelagranados@users.noreply.github.com>
2025-02-10 18:41:04 -0700

    add error logging for invoice creation and update operations

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -55,6 +55,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
     await sql`INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
   } catch (error) {
+    console.error(error);
     return {
       message: 'Database Error: Failed to Create Invoice.',
     };
@@ -87,6 +88,7 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
     WHERE id = ${id}
   `;
   } catch (error) {
+    console.error(error);
     return { message: 'Database Error: Failed to Update Invoice.' };
   }
 
