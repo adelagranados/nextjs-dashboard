2a050f015b37b09b310f6a2073b77b6efd95b054
adelagranados <55199072+adelagranados@users.noreply.github.com>
2025-02-11 19:31:49 -0700

    refactor user fetching logic in authentication module

--- auth.ts
+++ auth.ts
@@ -8,8 +8,8 @@ import { sql } from "@vercel/postgres";
 
 async function getUser(email: string): Promise<User | undefined> {
   try {
-    const user = await sql<User[]>`SELECT * FROM users WHERE email=${email}`;
-    return user.rows[0];
+    const result = await sql<User>`SELECT * FROM users WHERE email=${email}`;
+    return result.rows[0];
   } catch (error) {
     console.error("Failed to fetch user:", error);
     throw new Error("Failed to fetch user.");
